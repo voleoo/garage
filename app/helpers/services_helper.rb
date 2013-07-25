d8b3946ab2e0b6e764e7577c3e6cedecde2517c5
Viktor Leonets <4405511@gmail.com>
2013-07-25 08:30:27 +0300

    all good

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,2 +1,7 @@
 module ServicesHelper
+
+  def expert_users
+    @expert_users ||= Role.find_by_role('expert').users
+  end
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,2 @@
 module UsersHelper
-
-  private
-  def experts
-    @experts ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  helper_method :experts
 end
